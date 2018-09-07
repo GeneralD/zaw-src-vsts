fd33f9fdaae7c6d65ada51910f386e9faa64276b
GeneralD <yumejustice@gmail.com>
2018-09-08 00:56:53 +0900

    [from now] 2018/09/08 00:56:53
    diff --git a/src/variables.zsh b/src/variables.zsh
    index 3d1bb55..224c763 100755
    --- a/src/variables.zsh
    +++ b/src/variables.zsh
    @@ -1,4 +1,5 @@
     #!/usr/bin/env zsh
    
    -ZAW_VSTS_INSTANCE=${ZAW_VSTS_INSTANCE:-`vsts configure -l | \grep instance | \awk '{ print $NF }'`} # set yours
    +ZAW_VSTS_INSTANCE=${ZAW_VSTS_INSTANCE:-`vsts configure -l | /usr/bin/grep
    + instance | /usr/bin/awk '{ print $NF }'`}
     ZAW_VSTS_CLONE_METHOD=${ZAW_VSTS_CLONE_METHOD:-'ssh'} # ssh or https

--- src/variables.zsh
+++ src/variables.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
 
-ZAW_VSTS_INSTANCE=${ZAW_VSTS_INSTANCE:-`vsts configure -l | \grep instance | \awk '{ print $NF }'`} # set yours
+ZAW_VSTS_INSTANCE=${ZAW_VSTS_INSTANCE:-`vsts configure -l | /usr/bin/grep
+ instance | /usr/bin/awk '{ print $NF }'`}
 ZAW_VSTS_CLONE_METHOD=${ZAW_VSTS_CLONE_METHOD:-'ssh'} # ssh or https
